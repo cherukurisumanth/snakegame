0f017b2e8f844f5c2ac73c88f11a29d0d6c55c14
cherukuri sumanth <sumanth405@gmail.com>
2021-04-11 11:04:57 +0530

    second day commit

--- javaScript/snake.js
+++ javaScript/snake.js
@@ -1,17 +1,17 @@
-export const SNAKE_SPEED = 1;
-const snakeBody = [
-  { x: 10, y: 11 },
-  { x: 11, y: 11 },
-  { x: 12, y: 11 },
-];
+import { getInputDirection } from "./input.js";
+
+export const SNAKE_SPEED = 5;
+const snakeBody = [{ x: 11, y: 11 }];
+let newSegments = 0;
 
 export function update() {
+  const inputDirection = getInputDirection();
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
   }
 
-  snakeBody[0].x += 1;
-  snakeBody[0].y += 0;
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
 }
 
 export function draw(gameBoard) {
@@ -21,5 +21,15 @@ export function draw(gameBoard) {
     snakeElement.style.gridColumnStart = segment.x;
     snakeElement.classList.add("snake");
     gameBoard.appendChild(snakeElement);
+  });
+}
+
+export function expandSnake(amount) {
+  newSegments += amount;
+}
+
+export function onSnake(position) {
+  return snakeBody.some((segment) => {
+    return equalPositions(segment, positions);
   });
 }
